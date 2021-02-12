6f2d191ef67c9f3451c907fb2b07cf6439b5a4a5
Alexandre Mutel <alexandre_mutel@live.com>
2021-02-12 08:23:04 +0100

    Add crypto_lock crypto_unlock tests

--- src/Monocypher.Tests/Tests.cs
+++ src/Monocypher.Tests/Tests.cs
@@ -1,4 +1,6 @@
-using Microsoft.VisualBasic;
+using System;
+using System.Security.Cryptography;
+using System.Text;
 using NUnit.Framework;
 
 using static Monocypher.monocypher;
@@ -7,18 +9,49 @@ namespace Monocypher.Tests
 {
     public class Tests
     {
-
         [Test]
-        public void Test1()
+        public void SimpleBlake2Init()
         {
             crypto_blake2b_ctx ctx = default;
             crypto_blake2b_init(ref ctx);
+            Assert.AreNotEqual(0, ctx.hash_size.Value.ToInt32(), "Invalid hash_size returned from blake2b_init");
         }
 
-
-        public void TestCryptoLockUnlock()
+        [Test]
+        public void CryptoLockUnlock()
         {
-            // TODO
+            Span<byte> mac = stackalloc byte[16];
+            Span<byte> cipherText = stackalloc byte[16];
+            Span<byte> inputText = stackalloc byte[16];
+            inputText[0] = (byte)'a';
+            inputText[1] = (byte)'b';
+            inputText[2] = (byte)'c';
+            inputText[3] = (byte)'d';
+
+            Span<byte> key = stackalloc byte[32];
+            RNGCryptoServiceProvider.Fill(key);
+            Span<byte> nonce = stackalloc byte[24];
+            RNGCryptoServiceProvider.Fill(nonce);
+
+            crypto_lock(mac, cipherText, key, nonce, inputText);
+
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                builder.Append($"{cipherText[i]:X2}");
+            }
+            Console.WriteLine($"cipher: {builder}");
+            
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                Assert.AreNotEqual(inputText[i], cipherText[i], $"Crypto failed: Invalid byte found at position {i}");
+            }
+
+            Span<byte> outputText = stackalloc byte[16];
+            crypto_unlock(outputText, key, nonce, mac, cipherText);
+
+            Assert.True(outputText.SequenceEqual(inputText), "crypto_unlock failed. Spans are different");
         }
     }
 }
